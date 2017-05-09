2e0ba86a11cc79a97284f0b8b3e82084c1909905
jarema <jarema.leskiw@gmail.com>
2017-05-09 18:20:30 +0200

    add several tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -16,6 +16,27 @@ class TestDummy(unittest.TestCase):
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
 
+        retcode = main(("--year", "20011", "--month", "1", "--day", "3"))
+        self.assertEqual(retcode, None)
+
+        retcode = main(("--year", "2001", "--month", "13", "--day", "3"))
+        self.assertEqual(retcode, None)
+
+        retcode = main(("--year", "2001", "--month", "31", "--day", "32"))
+        self.assertEqual(retcode, None)
+
+        weekday = calculate(2017, 5, 9)
+        self.assertEqual(weekday, 1)
+
+        weekday = calculate(2017, 4, 31)
+        self.assertFalse(weekday == 0)
+
+        weekday = calculate(2016, 2, 29)
+        self.assertEqual(weekday, 0)
+
+        weekday = calculate(2017, 2, 29)
+        self.assertEqual(weekday, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
